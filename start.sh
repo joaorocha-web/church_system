4b5edb7136a6ec17294583a980fd0cc12abb6f34
joaorocha-web <jvrochajf@eaportal.org>
2025-12-16 14:20:11 -0300

    Alteração para deploy

--- start.sh
+++ start.sh
@@ -2,5 +2,10 @@
 
 php artisan migrate --force
 php artisan optimize:clear
+php artisan config:clear
+php artisan cache:clear
+php artisan view:clear
+php artisan route:clear
 
+chmod -R 775 storage bootstrap/cache
 exec php-fpm
